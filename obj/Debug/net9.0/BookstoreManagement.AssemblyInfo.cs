9e61171c6145cf59eea3119ebcead1d7d3d23f97
Tuan Thai <titofood17122004@gmail.com>
2025-10-08 08:36:51 +0700

    Fix Folder

--- obj/Debug/net9.0/BookstoreManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookstoreManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd1fd8c80fb805e4a167eed8106b9f13955edeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db75f734202a9ed010118ba9e9eebee5405e12c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
